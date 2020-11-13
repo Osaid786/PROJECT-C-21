cfc9bd441bdd75d7f86633515ab61fecf79993d8
Osaid786 <73431728+Osaid786@users.noreply.github.com>
2020-11-13 21:52:37 +0530

    Add files via upload

---
+++ collision.js
@@ -0,0 +1,11 @@
+function hasCollided(lbullet,lwall) 
+{
+    bulletRightEdge=lbullet.x+lbullet.width;
+    wallLeftEdge=lwall.x;
+    if (bullet.x>=wallLeftEdge) {
+        return=true
+    }
+    
+        return=false
+    
+}
